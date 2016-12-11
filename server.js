5d857698ecd139a6084ef55932315574b35596a3
PhanurutCh <phanurut9890@gmail.com>
2016-12-11 11:32:14 +0700

    try

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/webhook/', function (req, res) {
     }
     if (event.postback) {
       let text = JSON.stringify(event.postback)
-      sendTextMessage(sender, 'Postback received: ' + text.substring(0, 200), token)
+      sendTextMessage(sender, 'Postback received: Welcome ' + text.substring(0, 200), token)
       continue
     }
   }
