de71854b6a988e1afaeb862ca6dee7f3500799f3
PhanurutCh <phanurut9890@gmail.com>
2016-12-11 11:46:53 +0700

    try

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/webhook/', function (req, res) {
     }
     if (event.postback) {
       let text = JSON.stringify(event.postback)
-      sendTextMessage(sender, 'Postback received: กรุณาใส่ชื่อเมือง ' , token)
+      sendTextMessage(sender, 'กรุณาใส่ชื่อเมือง ' , token)
       continue
     }
   }
